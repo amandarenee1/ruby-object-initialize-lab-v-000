d303dedc3150d6d5f1b4e80b594018e72909e7dc
Amanda Wingate <arw0219@gmail.com>
2018-01-28 23:09:22 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
+  def initialize(beyonce)
+    @beyonce = beyonce
+  end
 
+  def beyonce = (beyonce)
+    @beyonce = beyonce
+  end
+
+  def beyonce
+    @beyonce
+  end
 end
+
+beyonce = Person.new("Beyonce")
